7fed1ef99f461224de40a142995f9e3af83da032
Anna <34191811+404unf@users.noreply.github.com>
2023-06-30 19:22:54 -0700

    Saving

--- src/index.js
+++ src/index.js
@@ -13,3 +13,64 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+// Begin
+// When page loads, fetch toys from the database 
+addEventListener('DOMContentLoaded',fetchToys)
+
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+    .then(response=>response.json())
+    .then(data=>makeCard(data))
+}
+
+function makeCard(data) {
+  for(let i=0;i<data.length;i++){
+    const collectionDiv = document.getElementById('toy-collection')
+    const newDiv = document.createElement('div')
+    newDiv.className = 'card'
+
+    const newHeader = document.createElement('h2')
+    newHeader.textContent = data[i].name
+    newDiv.append(newHeader)
+
+    const newImg = document.createElement('img')
+    newImg.className = 'toy-avatar'
+    newImg.src = data[i].image
+    newDiv.append(newImg)
+
+    const newP = document.createElement('p')
+    newP.textContent = `${data[i].likes} Likes`
+    newDiv.append(newP)
+
+    const newButton = document.createElement('button')
+    newButton.className = 'like-btn'
+    newButton.id = data[i].id
+    newButton.textContent = 'Like'
+    newDiv.append(newButton)
+
+    collectionDiv.append(newDiv)
+  }
+}
+
+// When a user submits the form a new toy is added to the database
+
+const form = document.getElementsByClassName('add-toy-form')
+form.addEventListener('submit',addNewToy)
+
+function addNewToy() {
+  const response = fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      'name': this.name,
+      'image': this.value,
+      'likes':0
+    })
+  })
+}
